8df009eb826af7cdfdf38c0379c0e9629046a6ac
Ariana Fairbanks <arianafairbanks@gmail.com>
2015-11-06 13:06:20 -0700

    Adjusted alignment.

--- app/src/main/java/ctec/textingappactivity/UpdateTextingActivity.java
+++ app/src/main/java/ctec/textingappactivity/UpdateTextingActivity.java
@@ -1,6 +1,8 @@
 package ctec.textingappactivity;
 
-
+/**
+ * Imports all the stuff we think we will need.
+ */
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,21 +15,25 @@ import android.widget.Toast;
 import android.telephony.SmsManager;
 import java.util.ArrayList;
 
-
-
 /**
- * @author Ariana Fairbanks
+ *  Declares GUI elements.
  */
+
 public class UpdateTextingActivity extends Activity
 {
 
-    private EditText RecieveField;  // Not used yet.
     private EditText SendField;
     private EditText NumberField;
     private Button Send;
     private Button Thomas;
     private Button Clayton;
+    private Button Unknown;
     private ArrayList<String> ContactList;
+
+    /**
+     * Instantiates GUI elements and calls the setupListeners() method.
+     * @param savedInstanceState Sets the current state as the last saved instance.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -40,33 +46,42 @@ public class UpdateTextingActivity extends Activity
         Send = (Button) findViewById(R.id.Send);
         Thomas = (Button) findViewById (R.id.Thomas);
         Clayton = (Button) findViewById (R.id.Clayton);
+        Unknown = (Button) findViewById (R.id.unknown);
         ContactList = (new ArrayList());
 
         ContactList.add("Hello Thomas.");
         ContactList.add("Hello Clayton.");
+        ContactList.add("Greetings from CTEC!");
 
 
 
         setupListeners();
     }
 
+    /**
+     * @param menu Inflates the menu and, if available, adds items to the action bar.
+     * @return Returns true.
+     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_update_texting, menu);
         return true;
     }
 
+    /**
+     *
+     * @param item
+     * @return
+     */
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings)
         {
             return true;
@@ -75,10 +90,20 @@ public class UpdateTextingActivity extends Activity
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Sets up listeners.
+     */
+
     private void setupListeners()
     {
         Send.setOnClickListener(new View.OnClickListener()
         {
+
+            /**
+             *
+             * @param currentView
+             */
+
             @Override
             public void onClick(View currentView)
             {
@@ -98,10 +123,15 @@ public class UpdateTextingActivity extends Activity
             }
         });
 
+        /**
+         * Automatically inputs Thomas' contact information and a message for him when the 'Thomas' button is pressed.
+         */
+
         Thomas.setOnClickListener(new View.OnClickListener()
         {
 
-            public void onClick(View currentView) {
+            public void onClick(View currentView)
+            {
                 NumberField.setText("8018088495");
                 SendField.setText(ContactList.get(0).toString());
 
@@ -109,12 +139,31 @@ public class UpdateTextingActivity extends Activity
 
         });
 
+        /**
+         * Automatically inputs Clayton's contact information and a message for him when the 'Clayton' button is pressed.
+         */
 
         Clayton.setOnClickListener(new View.OnClickListener()
         {
-            public void onClick(View currentView) {
-                NumberField.setText("8018088495");
-                SendField.setText(ContactList.get(1).toString());
+        public void onClick(View currentView)
+        {
+            NumberField.setText("8018088495");
+            SendField.setText(ContactList.get(1).toString());
+
+        }
+
+        });
+
+        /**
+         * Automatically inputs contact information and a message when the button is pressed.
+         */
+
+        Unknown.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View currentView)
+            {
+                NumberField.setText("8015607545");
+                SendField.setText(ContactList.get(2).toString());
 
             }
 
@@ -122,6 +171,11 @@ public class UpdateTextingActivity extends Activity
         });
     }
 
+    /**
+     * Uses the SMS Manager to send the SMS message.
+     * @param messageAddress Where you are sending the message.
+     * @param messageContent What is in the message.
+     */
 
     private void sendSMS(String messageAddress, String messageContent)
 
